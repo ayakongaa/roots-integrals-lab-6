5f72ce103cebe6424cab594898e222d79b219e63
Aya <kongaaaya@icloud.com>
2026-05-18 11:06:22 +0300

    Update README.md

--- submissions/kongaa/src/funcs.asm
+++ submissions/kongaa/src/funcs.asm
@@ -4,18 +4,9 @@ section .data
     fourteen dq 14.0
 
 section .text
-%ifdef WINDOWS
-    global _f1
-    global _f2
-    global _f3
-%define f1 _f1
-%define f2 _f2
-%define f3 _f3
-%else
     global f1
     global f2
     global f3
-%endif
 
 f1:
     push ebp
